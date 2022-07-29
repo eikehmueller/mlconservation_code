497e42d560acfe50b31627d2c4f62af2eb3a3a23
eikehmueller <eike.h.mueller@gmail.com>
2022-07-29 14:03:29 +0200

    Refactoring: derive from base class to allow the reuse of the save/load methods for general models.

--- src/nn_models.py
+++ src/nn_models.py
@@ -7,64 +7,12 @@ from auxilliary import ndarrayDecoder, ndarrayEncoder
 from lagrangian_dynamical_system import LagrangianDynamicalSystem
 
 
-class XYModelNNLagrangian(tf.keras.layers.Layer):
-    """Neural network representation of Lagrangian for the XY model
+class NNLagrangian(tf.keras.layers.Layer):
+    """Base class for neural network based Lagrangians"""
 
-    Rotational invariance
-
-        L(q_0,...,q_{d-1};qdot_0,...,qdot_{d-1})
-          = L(q_0+phi,...,q_{d-1}+phi;qdot_0,...,dot_{d-1})
-
-    is exactly enforced by taking the input
-
-      y = (q_0,q_1,...,q_{d-1},qdot_0,qdot_1,...,qdot_{d-1}) where
-
-    and converting it to a 2d dimensional vector
-
-      z = (q_0-q_{d-1},q_1-q_0,...,q_{d-1}-q_{d-2},qdot_1,...,qdot_{d-1})
-
-    :arg dim: dimension d = number of spins
-    :arg rotation_invariant: enforce rotational invariance
-    :arg shift_invariant: enforce shift invariance
-    """
-
-    def __init__(self, dim, rotation_invariant=True, shift_invariant=True):
-        super(XYModelNNLagrangian, self).__init__()
-        self.dim = dim
-        self.rotation_invariant = rotation_invariant
-        self.shift_invariant = shift_invariant
-        self.dense_layers = [
-            tf.keras.layers.Dense(8, activation="tanh"),
-            tf.keras.layers.Dense(8, activation="tanh"),
-            tf.keras.layers.Dense(1, use_bias=False),
-        ]
-
-    def call(self, inputs):
-        """Evaluate the Lagrangian for a given vector (q,qdot)
-
-        :arg inputs: 2d-dimensional phase space vector (q,qdot)
-        """
-        noffsets = self.dim if self.shift_invariant else 1
-        outputs = []
-        for offset in range(noffsets):
-            x = inputs
-            # Shift by offset
-            q_qdot = tf.unstack(inputs, axis=1)
-            q = tf.roll(tf.stack(q_qdot[: self.dim], axis=1), offset, axis=1)
-            qdot = tf.roll(tf.stack(q_qdot[self.dim :], axis=1), offset, axis=1)
-            x = tf.concat([q, qdot], axis=1)
-            if self.rotation_invariant:
-                # replace q_j by q_j - q_{j-1} to take rotational symmetry
-                # into account
-                q_qdot = tf.unstack(x, axis=1)
-                q = q_qdot[: self.dim]
-                qdot = q_qdot[self.dim :]
-                dq = [q[k] - q[(k + self.dim - 1) % self.dim] for k in range(self.dim)]
-                x = tf.stack(dq + qdot, axis=1)
-            for layer in self.dense_layers:
-                x = layer(x)
-            outputs.append(x)
-        return 1.0 / noffsets * tf.add_n(outputs)
+    def __init__(self, **kwargs):
+        super(NNLagrangian, self).__init__(**kwargs)
+        self.dense_layers = []
 
     def save(self, filepath, overwrite=True):
         """Save the model to the specified directory
@@ -126,6 +74,66 @@ class XYModelNNLagrangian(tf.keras.layers.Layer):
         for layer in model.dense_layers:
             layer.set_weights(layer_weights[layer.name])
         return model
+
+
+class XYModelNNLagrangian(NNLagrangian):
+    """Neural network representation of Lagrangian for the XY model
+
+    Rotational invariance
+
+        L(q_0,...,q_{d-1};qdot_0,...,qdot_{d-1})
+          = L(q_0+phi,...,q_{d-1}+phi;qdot_0,...,dot_{d-1})
+
+    is exactly enforced by taking the input
+
+      y = (q_0,q_1,...,q_{d-1},qdot_0,qdot_1,...,qdot_{d-1}) where
+
+    and converting it to a 2d dimensional vector
+
+      z = (q_0-q_{d-1},q_1-q_0,...,q_{d-1}-q_{d-2},qdot_1,...,qdot_{d-1})
+
+    :arg dim: dimension d = number of spins
+    :arg rotation_invariant: enforce rotational invariance
+    :arg shift_invariant: enforce shift invariance
+    """
+
+    def __init__(self, dim, rotation_invariant=True, shift_invariant=True, **kwargs):
+        super(XYModelNNLagrangian, self).__init__(**kwargs)
+        self.dim = dim
+        self.rotation_invariant = rotation_invariant
+        self.shift_invariant = shift_invariant
+        self.dense_layers = [
+            tf.keras.layers.Dense(4, activation="tanh"),
+            tf.keras.layers.Dense(4, activation="tanh"),
+            tf.keras.layers.Dense(1, use_bias=False),
+        ]
+
+    def call(self, inputs):
+        """Evaluate the Lagrangian for a given vector (q,qdot)
+
+        :arg inputs: 2d-dimensional phase space vector (q,qdot)
+        """
+        noffsets = self.dim if self.shift_invariant else 1
+        outputs = []
+        for offset in range(noffsets):
+            x = inputs
+            # Shift by offset
+            q_qdot = tf.unstack(inputs, axis=1)
+            q = tf.roll(tf.stack(q_qdot[: self.dim], axis=1), offset, axis=1)
+            qdot = tf.roll(tf.stack(q_qdot[self.dim :], axis=1), offset, axis=1)
+            x = tf.concat([q, qdot], axis=1)
+            if self.rotation_invariant:
+                # replace q_j by q_j - q_{j-1} to take rotational symmetry
+                # into account
+                q_qdot = tf.unstack(x, axis=1)
+                q = q_qdot[: self.dim]
+                qdot = q_qdot[self.dim :]
+                dq = [q[k] - q[(k + self.dim - 1) % self.dim] for k in range(self.dim)]
+                x = tf.stack(dq + qdot, axis=1)
+            for layer in self.dense_layers:
+                x = layer(x)
+            outputs.append(x)
+        return 1.0 / noffsets * tf.add_n(outputs)
 
     def get_config(self):
         """Get the model configuration"""
