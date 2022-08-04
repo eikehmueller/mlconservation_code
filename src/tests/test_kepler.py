e38bdc560e6d47c348b73df808e077ce7568219a
eikehmueller <eike.h.mueller@gmail.com>
2022-08-04 18:56:42 +0200

    Added test which checks that the acceleration from the analytical solution is consistent with the one from the dynamical system

---
+++ src/tests/test_kepler.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pytest
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from kepler import KeplerSolution
+from dynamical_system import KeplerSystem
+
+"""Tests for the Kepler solution
+
+Check that the analytical solution is consistent with the corresponding dynamical system
+"""
+
+
+def test_kepler_acceleration_analytical():
+    """Check that the acceleration computed with the analytical solution class
+    agrees with the acceleration from the corresponding dynamical system"""
+    mass = 0.983
+    alpha = 1.32
+    excentricity = 0.76
+    energy = -0.98
+    phi0 = 3.5
+    phi = 1.8
+    kepler_solution = KeplerSolution(
+        mass=mass, alpha=alpha, excentricity=excentricity, energy=energy, phi0=phi0
+    )
+    dynamical_system = KeplerSystem(mass=mass, alpha=alpha)
+    q = kepler_solution.position(phi)
+    qdot = kepler_solution.velocity(phi)
+    q_qdot = np.concatenate([q, qdot], axis=0)
+    acc_analytical = kepler_solution.acceleration(phi)
+    acc = dynamical_system.call(q_qdot)
+    tolerance = 1.0e-6
+    assert np.linalg.norm(acc - acc_analytical) < tolerance
