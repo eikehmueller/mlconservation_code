41a417f2367c3275f175af092e2342c07e1bc41b
eikehmueller <eike.h.mueller@gmail.com>
2022-08-05 13:33:16 +0200

    Tidying up of main training script, added support for all three systems

--- src/train_model.py
+++ src/train_model.py
@@ -1,13 +1,17 @@
 """Main script for training Neural network model
 
-Reads parameters from the file passed as a command line argument
+Parameters are set via a json file which is passed as a command line argument:
+
+python train_model.py --parameterfile=PARAMETERFILE
+
+If no parameter filename is given, it defaults to training_parameters.py.
 """
 
-import sys
 import json
+import argparse
 import tensorflow as tf
 
-from data_generator import DynamicalSystemDataGenerator
+from data_generator import DynamicalSystemDataGenerator, KeplerDataGenerator
 from dynamical_system import DoubleWellPotentialSystem, TwoParticleSystem
 from nn_models import (
     SingleParticleNNLagrangian,
@@ -18,48 +22,69 @@ from initializer import (
     SingleParticleConstantInitializer,
     TwoParticleConstantInitializer,
 )
+from kepler import KeplerSolution
 
-if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv} PARAMETERFILE")
-    sys.exit(-1)
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--parameterfile",
+    dest="parameterfile",
+    action="store",
+    default="training_parameters.json",
+    help="name of json file with training parameters",
+)
+cmdline_args = parser.parse_args()
 
 print("==========================================")
 print("     training neural network model")
 print("==========================================")
 
-parameter_filename = sys.argv[1]
-with open(parameter_filename, "r", encoding="utf-8") as json_data:
+with open(cmdline_args.parameterfile, "r", encoding="utf-8") as json_data:
     parameters = json.load(json_data)
     json_data.close()
 
 
-def pretty_print(d, indent):
-    """Print out parameters with indents"""
+def pretty_print(d, indent=0):
+    """Print out dictionary with indents
+
+    :arg d: dictionary to print
+    :arg indent: indent to use
+    """
     for key, value in d.items():
         if isinstance(value, dict):
             print(indent * " " + str(key))
-            pretty_print(value, indent + 2)
-        print(indent * " " + str(key) + " : " + str(value))
+            pretty_print(value, indent + 4)
+        else:
+            print(indent * " " + str(key) + " : " + str(value))
 
 
-pretty_print(parameters, 2)
+print("==== parameters ====")
+print("reading parameters from " + cmdline_args.parameterfile)
+pretty_print(parameters)
+print("")
 
 # Set training parameters passed from file
 EPOCHS = parameters["epochs"]
 STEPS_PER_EPOCH = parameters["steps_per_epoch"]
 BATCH_SIZE = parameters["batch_size"]
-SHUFFLE_BUFFER_SIZE = 32 * BATCH_SIZE
+SHUFFLE_BUFFER_SIZE = 4 * BATCH_SIZE
 
 rotation_invariant = bool(parameters["rotation_invariant"])
 translation_invariant = bool(parameters["translation_invariant"])
 reflection_invariant = bool(parameters["reflection_invariant"])
 
-# Select system
+# ---- Select system ----
 if parameters["system"] == "TwoParticle":
     dim_space = parameters["two_particle"]["dim_space"]
     dim = 2 * dim_space
     initializer = TwoParticleConstantInitializer(dim)
-    dynamical_system = TwoParticleSystem(dim_space)
+    dynamical_system = TwoParticleSystem(
+        dim_space,
+        mass1=parameters["two_particle"]["mass"][0],
+        mass2=parameters["two_particle"]["mass"][1],
+        mu=parameters["two_particle"]["mu"],
+        kappa=parameters["two_particle"]["kappa"],
+    )
     nn_lagrangian = TwoParticleNNLagrangian(
         dim_space,
         rotation_invariant=rotation_invariant,
@@ -69,26 +94,47 @@ if parameters["system"] == "TwoParticle":
 elif parameters["system"] == "DoubleWell":
     dim = parameters["double_well"]["dim"]
     initializer = SingleParticleConstantInitializer(dim)
-    dynamical_system = DoubleWellPotentialSystem(dim)
+    dynamical_system = DoubleWellPotentialSystem(
+        dim=dim,
+        mass=parameters["double_well"]["mass"],
+        mu=parameters["double_well"]["mu"],
+        kappa=parameters["double_well"]["kappa"],
+    )
     nn_lagrangian = SingleParticleNNLagrangian(
         dim,
+        rotation_invariant=rotation_invariant,
+        reflection_invariant=reflection_invariant,
+    )
+elif parameters["system"] == "Kepler":
+    nn_lagrangian = SingleParticleNNLagrangian(
+        3,
         rotation_invariant=rotation_invariant,
         reflection_invariant=reflection_invariant,
     )
 else:
     print("ERROR: unknown system :" + parameters["system"])
 
-# Create data generator
-data_generator = DynamicalSystemDataGenerator(
-    dynamical_system,
-    initializer,
-    re_initialize=False,
-    sigma=parameters["sigma"],
-    tinterval=0.1,
-)
+# ---- Create data generator ----
+if parameters["system"] in ["DoubleWell", "TwoParticle"]:
+    data_generator = DynamicalSystemDataGenerator(
+        dynamical_system,
+        initializer,
+        re_initialize=False,
+        sigma=parameters["sigma"],
+        tinterval=0.1,
+    )
+else:
+    kepler_solution = KeplerSolution(
+        mass=parameters["kepler"]["mass"],
+        alpha=parameters["kepler"]["alpha"],
+        excentricity=parameters["kepler"]["excentricity"],
+        energy=parameters["kepler"]["energy"],
+    )
+    data_generator = KeplerDataGenerator(kepler_solution, sigma=parameters["sigma"])
+
 train_batches = data_generator.dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 
-# Construct NN model
+# ---- Construct NN model ----
 model = LagrangianModel(nn_lagrangian)
 
 learning_rate = tf.keras.optimizers.schedules.CosineDecay(
@@ -97,7 +143,7 @@ learning_rate = tf.keras.optimizers.schedules.CosineDecay(
     alpha=1.0e-2,
 )
 
-# Compile model
+# ---- Compile model ----
 model.compile(
     loss="mse",
     metrics=[],
@@ -106,7 +152,7 @@ model.compile(
 log_dir = "./tb_logs/"
 tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 
-# Fit model
+# ---- Fit model ----
 result = model.fit(
     train_batches,
     epochs=EPOCHS,
